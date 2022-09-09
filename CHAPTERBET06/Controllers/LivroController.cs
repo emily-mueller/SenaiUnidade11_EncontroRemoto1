45964e9694d74083ae752a4fcc6f536438220125
emily-mueller <emily_mueller@id.uff.br>
2022-09-09 02:45:55 -0300

    API

---
+++ CHAPTERBET06/Controllers/LivroController.cs
@@ -0,0 +1,32 @@
+﻿using CHAPTERBET06.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CHAPTERBET06.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+
+        public LivroController(LivroRepository livroRep)
+        {
+            _livroRepository = livroRep;
+        }
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_livroRepository.Listar());
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ CHAPTERBET06/Repositories/LivroRepository.cs
@@ -0,0 +1,20 @@
+﻿using CHAPTERBET06.Contexts;
+using CHAPTERBET06.Models;
+
+namespace CHAPTERBET06.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly SqlContext _context;
+        public LivroRepository(SqlContext context)
+        {
+            _context = context;
+        }
+        
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+    }
+}
